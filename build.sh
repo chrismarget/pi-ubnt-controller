74153278fbffc8f26ae225ac7177dfc699a29d43
cmarget <cmarget@mutualink.net>
2020-09-14 15:37:04 -0400

    build script project dir detection

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
 FETCHED="$(dirname $0)/.fetched_submodule"
 if [ ! -f "$FETCHED" ]
@@ -6,3 +6,7 @@ then
   git submodule update --init
   touch "$FETCHED"
 fi
+
+PROJECT_DIR="$(dirname $0)"
+PROJECT_DIR="$(cd $PROJECT_DIR; pwd)"
+echo PROJECT_DIR $PROJECT_DIR
