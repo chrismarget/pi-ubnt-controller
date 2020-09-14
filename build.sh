a98864f39b93d877f6a9d040d5ae783f7e582dfc
cmarget <cmarget@mutualink.net>
2020-09-14 15:44:19 -0400

    pass build options to underlying build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 FETCHED="$(dirname $0)/.fetched_submodule"
 if [ ! -f "$FETCHED" ]
@@ -8,5 +8,7 @@ then
 fi
 
 PROJECT_DIR="$(dirname $0)"
-PROJECT_DIR="$(cd $PROJECT_DIR; pwd)"
-echo PROJECT_DIR $PROJECT_DIR
+export PROJECT_DIR="$(cd $PROJECT_DIR; pwd)"
+
+APPLIANCE_BUILD_SCRIPT=${PROJECT_DIR}/raspbian-appliance/build.sh
+[ -x $APPLIANCE_BUILD_SCRIPT ] && echo foo && $APPLIANCE_BUILD_SCRIPT $*
