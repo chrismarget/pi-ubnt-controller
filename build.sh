4ae42f6d13125943f03d2b88c6152ee085a22ad6
cmarget <cmarget@mutualink.net>
2020-09-18 12:06:50 -0400

    move cache dir to project

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ fi
 
 PROJECT_DIR="$(dirname $0)"
 export PROJECT_DIR="$(cd $PROJECT_DIR; pwd)"
+export CACHE_DIR="${PROJECT_DIR}/.build.cache"
 
 APPLIANCE_BUILD_SCRIPT=${PROJECT_DIR}/raspbian-appliance/build.sh
 [ -x $APPLIANCE_BUILD_SCRIPT ] && $APPLIANCE_BUILD_SCRIPT $*
